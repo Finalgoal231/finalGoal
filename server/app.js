a319ebb78be51ede8c72a32d832246c617639287
shine <shine401020@gmail.com>
2024-05-24 19:40:20 +0900

    fixed: bug error in server

--- server/app.js
+++ server/app.js
@@ -1,14 +1,14 @@
 // imports modules & dependencies
-const express = require('express');
-const env = require('dotenv');
-const favicon = require('serve-favicon');
-var path = require('path');
-var cors = require('cors');
-const passport = require('passport');
+const express = require("express");
+const env = require("dotenv");
+const favicon = require("serve-favicon");
+var path = require("path");
+var cors = require("cors");
+const passport = require("passport");
 
 // imports routes, middleware, and configs
-const routes = require('./src/routes');
-const { notFoundRoute, errorHandler } = require('./src/configs/errorHandler');
+const routes = require("./src/routes");
+const { notFoundRoute, errorHandler } = require("./src/configs/errorHandler");
 
 // loads environment variables from .env file
 env.config();
@@ -17,18 +17,17 @@ env.config();
 const app = express();
 
 // application database connection establishment
-const connectDatabase = require('./src/db/connect');
+const connectDatabase = require("./src/db/connect");
 // const passPort = require("./src/configs/passPort");
 connectDatabase();
-const mongoose = require('mongoose');
 // corss-origin-allow-all
 app.use(cors());
 
 // sets favicon in routes
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 
 // sets static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // parse requests of content-type - application/json
 app.use(express.json());
@@ -37,16 +36,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(passport.initialize());
-require('./src/configs/passport')(passport);
+require("./src/configs/passport")(passport);
 // sets default route
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res
     .status(200)
-    .json({ message: 'Welcome to App Node.js application backend.' });
+    .json({ message: "Welcome to App Node.js application backend." });
 });
 
 // api routes
-app.use('/api', routes);
+app.use("/api", routes);
 
 // 404 - not found error handler
 app.use(notFoundRoute);
@@ -54,10 +53,12 @@ app.use(notFoundRoute);
 // error handler
 app.use(errorHandler);
 
-const authCtr = require('./src/controllers/authController');
+const authCtr = require("./src/controllers/authController");
 authCtr.defaultAdmin();
 
 // app listens to defined port
 app.listen(process.env.APP_PORT, () => {
-  console.log(`Our Team-App backend server running on:  + ${process.env.APP_PORT}`);
+  console.log(
+    `Our Team-App backend server running on:  + ${process.env.APP_PORT}`
+  );
 });
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -4,8 +4,6 @@ const express = require("express");
 
 const authRoutes = require("./authRoute");
 const adminRoutes = require("./adminRoutes/adminRoutes");
-const shopRoutes = require("./eshopRoutes/sellerRoute");
-const studyRoutes = require("./studyRoutes");
 // email routes //
 
 //routes container
@@ -13,8 +11,6 @@ const router = express.Router();
 
 router.use("/auth", authRoutes);
 router.use("/admin", adminRoutes);
-router.use("/shop", shopRoutes);
-router.use("/study", studyRoutes);
 // email routes con//
 
 module.exports = router;
