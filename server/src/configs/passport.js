8df15d80d9413a4fab828f20520fd5bae779d10c
shine <shine401020@gmail.com>
2024-05-25 22:19:37 +0900

    fixed: complete middleware

--- server/src/configs/passport.js
+++ server/src/configs/passport.js
@@ -1,27 +1,21 @@
-const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
-const User = require('../models/userModel');
-const config = require('./config');
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const User = require("../models/userModel");
+const config = require("./config");
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = config.secretOrKey;
 
-module.exports = passport => {
+module.exports = (passport) => {
   passport.use(
     new JwtStrategy(opts, (jwt_payload, done) => {
-      console.log(opts.jwtFromRequest);
-      console.log(jwt_payload);
-      User.findById(jwt_payload.id)
-        .then(user => {
-          console.log('token', user);
-          if (user) {
-            return done(null, user);
-          }
+      User.findOne({username: jwt_payload.username})
+        .then((user) => {
+          console.log("token", user);
+          if (user) return done(null, user)
           return done(null, false);
         })
-        .catch(err => {
-          console.log(err);
-        });
-    }),
+        .catch((err) => {console.log(err)});
+    })
   );
 };
