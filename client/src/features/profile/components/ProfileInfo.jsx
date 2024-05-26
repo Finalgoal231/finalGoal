0184c403314dda5ac9561da6884875daca0fc2c0
shine <shine401020@gmail.com>
2024-05-27 01:44:27 +0900

    fixed: profile info

--- client/src/features/profile/components/ProfileInfo.jsx
+++ client/src/features/profile/components/ProfileInfo.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getUser } from "../../../redux/adminSlice";
+import moment from "moment";
 
 const ProfileInfo = () => {
   const dispatch = useDispatch();
@@ -12,20 +13,25 @@ const ProfileInfo = () => {
   useEffect(() => {
     dispatch(getUser(id));
   }, [dispatch, id]);
-
+  console.log(user);
   return (
     <>
-      <div className="flex justify-center gap-10">
-        <div className="w-1/3 h-32 border-4 flex flex-col items-center" >
-          <div className=""><img src="default.jpg" alt="Avatar" /></div>
-          <div className="font-bold">{user.username}</div>
-          <div>{user.name}</div>
-          <div>Join Date</div>
-          <div>Follows-Following</div>
-          <div>Follow</div>
+      {user.name !== undefined && (
+        <div className="flex justify-center gap-10">
+          <div className="w-1/3 h-auto border-4 flex flex-col items-center">
+            <div className="">
+              <img src={user.avatar} alt="Avatar" />
+            </div>
+            <div className="font-bold">{user.username}</div>
+            <div>{user.name}</div>
+            <div>{moment(user.createdAt).format("YYYY-MM-DD")}</div>
+            <div>{user.followers.length} Follower</div>
+            <div>{user.following.length} Following</div>
+            <div><button>Follow</button></div>
+          </div>
+          <div className="w-2/3">123</div>
         </div>
-        <div className="w-2/3">123</div>
-      </div>
+      )}
     </>
   );
 };
