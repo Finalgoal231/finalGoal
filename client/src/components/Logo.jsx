5924c0013d807be9646d36213d00aed84118c1ad
yonex <yandex@gmail.com>
2024-05-25 18:01:46 -0700

    fixed: Logo

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -2,12 +2,12 @@ function Logo() {
   return (
     <div className="flex flex-col justify-center items-center min-h-full bg-base-200 rounded-xl">
       <div className=" py-12">
-        <div className="max-w-md">
-          <h1 className="text-3xl text-center font-semibold mb-2">TODO</h1>
+        <div className="max-w-md flex flex-col justify-center items-center">
+          <h1 className="text-6xl text-center font-semibold mb-2 signHead">Blug Website</h1>
           <img
-            src="./assets/images/photo_2024-03-20_14-41-42.jpg"
+            src="default.png"
             alt=""
-            className="w-48 mt-12 rounded-3xl"
+            className="w-48 mt-6 rounded-3xl"
           ></img>
         </div>
       </div>
