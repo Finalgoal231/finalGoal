023dacb10e5ab422933b251d51da63d6bb85b666
luckystar <gentlemikeross@gmail.com>
2024-05-26 12:25:45 -0700

    password animation

--- client/src/features/settings/changepassword/index.js
+++ client/src/features/settings/changepassword/index.js
@@ -3,25 +3,26 @@ import TitleCard from "../../../components/Cards/TitleCard";
 import InputText from "../../../components/Input/InputText";
 import { useDispatch } from "react-redux";
 import { createPassword } from "../../../redux/authSlice";
+import "../../../index.css";
 
 const ChangePassword = () => {
   const dispatch = useDispatch();
   const [newData, setNewData] = useState({
     currentPassword: "",
     newpassword: "",
-    confirmpassword: ""
-  })
+    confirmpassword: "",
+  });
   const setHandlePassword = (e) => {
     console.log(newData);
-    setNewData({...newData, [e.target.name]: e.target.value});
-  }
+    setNewData({ ...newData, [e.target.name]: e.target.value });
+  };
 
   const setHandleSend = () => {
     dispatch(createPassword({ ...newData, complete: true }));
-};
+  };
   return (
     <div>
-      <TitleCard className="" title="Change password " topMargin="mt-10">
+      <TitleCard title="Change password " topMargin="mt-10">
         <div className="flex gap-20 mt-10">
           <div className=" rounded-full overflow-hidden h-60 w-60 ml-20 hover: cursor-pointer">
             <img src="./assets/avatar/avatar-17.jpg" />
@@ -31,36 +32,48 @@ const ChangePassword = () => {
             />
           </div>
           <div className="w-[70%] grid grid-cols-1 md:grid-cols-1 gap-20">
-             <InputText
-                name={"currentPassword"}
-                labelTitle={"Current password:"}
-                placeholder={"Input Title of Current Password"}
-                labelStyle={"text-[30px]"}
-                onChange={setHandlePassword}
+            <InputText
+              name={"currentPassword"}
+              labelTitle={"Current password:"}
+              placeholder={"Input Title of Current Password"}
+              labelStyle={"text-[30px]"}
+              onChange={setHandlePassword}
             />
-             <InputText
-                name={"newpassword"}
-                labelTitle={"New password:"}
-                placeholder={"Input Title of  New Password"}
-                labelStyle={"text-[30px]"}
-                onChange={setHandlePassword}
+            <InputText
+              name={"newpassword"}
+              labelTitle={"New password:"}
+              placeholder={"Input Title of  New Password"}
+              labelStyle={"text-[30px]"}
+              onChange={setHandlePassword}
             />
-             <InputText
-                name={"confirmpassword"}
-                labelTitle={"Confirm password:"}
-                placeholder={"Input Title of  Confirm Password"}
-                labelStyle={"text-[30px]"}
-                onChange={setHandlePassword}
+            <InputText
+              name={"confirmpassword"}
+              labelTitle={"Confirm password:"}
+              placeholder={"Input Title of  Confirm Password"}
+              labelStyle={"text-[30px]"}
+              onChange={setHandlePassword}
             />
           </div>
         </div>
-        <div className="mt-16">
-          <button className="btn btn-primary float-right mr-16 ml-16">
-            Cancel
-          </button>
-        </div>
-        <div className="mt-16">
-          <button className="btn btn-primary float-right" onClick={setHandleSend}>Save</button>
+        <div className="flex justify-end">
+          <div className="mt-16 mx-16">
+            <div className="flex items-center flex-col relative">
+            <button
+              className="btn btn-primary w-20 h-8"
+              onClick={setHandleSend}
+            >
+              Save
+            </button>
+            <div className="absolute passBtn"></div>
+
+            </div>
+          </div>
+          <div className="mt-16 mx-16">
+            <div className="flex items-center flex-col relative">
+              <button className="btn btn-primary w-20 h-8">Cancel</button>
+              <div className="absolute passBtn"></div>
+            </div>
+          </div>
         </div>
       </TitleCard>
     </div>
