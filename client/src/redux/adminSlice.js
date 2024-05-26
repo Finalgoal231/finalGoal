1e79da229dd3ccbda01f2a8710a6ac1b7d64c4fb
yonex <yandex@gmail.com>
2024-05-26 01:39:41 -0700

    fixed: usermanage read

--- client/src/redux/adminSlice.js
+++ client/src/redux/adminSlice.js
@@ -4,6 +4,10 @@ import axios from "axios";
 
 const base_url = "http://localhost:4000/api/admin";
 
+export const allUser = createAsyncThunk("allUser", async () => {
+  const res = await axios.get(base_url + "/user/all");
+  return res.data;
+});
 export const getAllCategory = createAsyncThunk("getAllCategory", async () => {
   const res = await axios.get(base_url + "/category/all");
   return res.data;
@@ -16,55 +20,49 @@ export const updateCategory = createAsyncThunk("editCategory", async (data) => {
   const res = await axios.put(base_url + `/category/${data._id}`, data);
   return res.data;
 });
-export const deleteCategory = createAsyncThunk(
-  "deleteCategory",
-  async (data) => {
-    const res = await axios.delete(base_url + `/category/${data._id}`);
-    return res.data;
-  }
-);
+export const deleteCategory = createAsyncThunk("delCategory", async (data) => {
+  const res = await axios.delete(base_url + `/category/${data._id}`);
+  return res.data;
+});
 
 export const adminSlice = createSlice({
   name: "admin",
   initialState: {
+    users: [],
     categories: [],
     category: {
       title: "",
       _id: "",
     },
-    msg: "",
     flag: false,
   },
   reducers: {
-    setPageTitle: (state, action) => {
-      state.pageTitle = action.payload.title;
-    },
     getCategory: (state, action) => {
       state.category.title = action.payload.val.title;
       state.category._id = action.payload.val._id;
     },
   },
   extraReducers: (builder) => {
+    builder.addCase(allUser.fulfilled, (state, action) => {
+      state.users = action.payload.users;
+    });
     builder.addCase(getAllCategory.fulfilled, (state, action) => {
       state.categories = action.payload.result;
       state.flag = true;
     });
     builder.addCase(createCategory.fulfilled, (state, action) => {
-      state.msg = action.payload.msg;
       state.flag = false;
     });
     builder.addCase(updateCategory.fulfilled, (state, action) => {
-      state.msg = action.payload.msg;
       state.category.title = "";
       state.flag = false;
     });
     builder.addCase(deleteCategory.fulfilled, (state, action) => {
-      state.msg = action.payload.msg;
       state.flag = false;
     });
   },
 });
 
-export const { getCategory, setFlag } = adminSlice.actions;
+export const { getCategory } = adminSlice.actions;
 
 export default adminSlice.reducer;
