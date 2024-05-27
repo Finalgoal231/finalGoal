9d83ef8163e3cf929d894536e26f5f52386b887b
yonex <yandex@gmail.com>
2024-05-27 01:01:18 -0700

    fixed: signup bug

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -3,9 +3,9 @@ function Logo() {
     <div className="flex flex-col justify-center items-center min-h-full bg-base-200 rounded-xl">
       <div className=" py-12">
         <div className="max-w-md flex flex-col justify-center items-center">
-          <h1 className="text-6xl text-center font-semibold mb-2 signHead">Blug Website</h1>
+          <h1 className="text-6xl text-center font-semibold mb-2 signHead">Blog Website</h1>
           <img
-            src="default.png"
+            src="default.jpg"
             alt=""
             className="w-48 mt-6 rounded-3xl"
           ></img>
